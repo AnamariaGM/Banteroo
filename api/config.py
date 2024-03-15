3091ba567174686de1009160d3dc66d0b977d7e8
AnamariaGM <anamariamorogan2992@gmail.com>
2024-03-15 11:04:03 +0000

    deleted the value of the custom_base_url

--- api/config.py
+++ api/config.py
@@ -1,2 +1,2 @@
-custom_base_url = "https://b9aa-2a02-c7c-8506-8300-3fc-f25-21f-2123.ngrok-free.app"  # Set your custom base URL here
-custom_base_domain = "b9aa-2a02-c7c-8506-8300-3fc-f25-21f-2123.ngrok-free.app"
+custom_base_url = ""  # Set your custom base URL here
+custom_base_domain = ""
